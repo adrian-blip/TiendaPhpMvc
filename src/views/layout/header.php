dac73874cfddf28b5439295c61ef039b584b8117
adrian-blip <adriancamiloguerra@gmail.com>
2025-03-02 23:26:20 +0100

    uodate

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tienda de camisetas</title>
-    <link rel="stylesheet" href="<?= base_url ?>/assets/css/styles.css">
+    <link rel="stylesheet" href="<?= base_url ?>assets/css/styles.css">
 </head>
 
 <body>
